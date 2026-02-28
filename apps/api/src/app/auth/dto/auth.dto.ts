936edb7ed627d57aae624af4fc02ad7fe3d47776
JomeAla <jomealawuru@hotmail.com>
2026-02-28 01:43:10 +0100

    fix_ts_config_and_types

--- apps/api/src/app/auth/dto/auth.dto.ts
+++ apps/api/src/app/auth/dto/auth.dto.ts
@@ -2,23 +2,23 @@ import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class RegisterDto {
   @IsEmail()
-  email: string;
+  email!: string;
 
   @IsNotEmpty()
   @IsString()
   @MinLength(8)
-  password: string;
+  password!: string;
 
   @IsNotEmpty()
   @IsString()
-  businessName: string;
+  businessName!: string;
 }
 
 export class LoginDto {
   @IsEmail()
-  email: string;
+  email!: string;
 
   @IsNotEmpty()
   @IsString()
-  password: string;
+  password!: string;
 }

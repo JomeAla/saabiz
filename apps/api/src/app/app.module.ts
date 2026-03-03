c678916b2a57c48016d04c335a9773fcb0e9651b
JomeAla <jomealawuru@hotmail.com>
2026-03-03 06:30:31 +0100

    payments-module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma.service';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, PaymentsModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
